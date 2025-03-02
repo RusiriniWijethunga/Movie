06ebb5130ab93426a6760d27f47690061711f0de
RusiriniWijethunga <95708918+RusiriniWijethunga@users.noreply.github.com>
2025-03-02 17:11:59 +0530

    change content

--- VIDLY/VIDLY/Controllers/MoviesController.cs
+++ VIDLY/VIDLY/Controllers/MoviesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VIDLY.Models;
 
 namespace VIDLY.Controllers
 {
@@ -12,7 +13,11 @@ namespace VIDLY.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Three Ediots" };
-            return View();
+            // return View(movie);
+            //return Content("Hello World");
+            // return HttpNotFound();
+            //return new EmptyResult();
+            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
         }
     }
 }
